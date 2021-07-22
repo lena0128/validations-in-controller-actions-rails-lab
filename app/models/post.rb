77d5f6c7a201986f3785cbd896d8768b4c87d9a1
Lena Hou <lenahou2013@gmail.com>
2021-07-21 22:59:24 -0400

    commit tree 89b7ea1c365408e641778620639fc653be121460

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence:true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) } 
+  # %w is a notation to write an array of strings separated by spaces instead of commas and without quotes around them.
+  validates :content, length: { minimum:100 }
 end
